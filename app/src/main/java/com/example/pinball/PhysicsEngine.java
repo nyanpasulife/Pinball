5702e9ec2d949c4e0d657208590a0e050f21d48e
nyanpasulife <xotpqnd@ajou.ac.kr>
2019-05-03 17:23:51 +0900

    a

--- app/src/main/java/com/example/pinball/PhysicsEngine.java
+++ app/src/main/java/com/example/pinball/PhysicsEngine.java
@@ -1,35 +1,54 @@
 package com.example.pinball;
 
-import android.util.Log;
-import android.view.SurfaceView;
-
 public class PhysicsEngine extends Thread{
     public PhysicsEngine(){
 
     }
 
-    @Override
-    public void run() {
-        while(true){
-            Log.d("tag", "running");
-        }
+    //    @Override
+    //    public void run() {
+    //        while(true){
+    //            Log.d("tag", "running");
+    //        }
+    //    }
+    //
+    //    // 두 오브젝트가 충돌하면 두 오브젝트의 행동함수를 호출되는 함수
+
+    private void collisionCheck(PhysicsInterface a, PhysicsInterface b){
+        a.collisionCheck(b);
     }
 
-    // 두 오브젝트가 충돌하면 두 오브젝트의 행동함수를 호출되는 함수
+
     private void isCollision(PhysicsEngine.PhysicsInterface a, PhysicsEngine.PhysicsInterface b){
-        a.beCollided();
-        b.beCollided();
+        a.beCollided(b);
     }
 /*
     public PhysicsInterface getView(){
         return ;
     }
 */
+
+
     public interface PhysicsInterface{
-        void beCollided();
+        boolean collisionCheck(PhysicsInterface x);
+        void beCollided(PhysicsInterface x);
+        void act(double px, double py);
+
+
+    }
+
+    public class Vector{
+        double X; double Y;
+        Vector(double x, double y){X =x, Y = y}
+        public Vector plus(Vector a, Vector b){
+            return new Vector(a.X+b.X, a.Y+b.Y);
+        }
+
     }
 
     public class Grid{
 
     }
+
+
 }
