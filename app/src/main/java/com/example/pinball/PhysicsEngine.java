810457643576d00fc947e54b408c5ea6b7c74f66
GS65 <char1909@ajou.ac.kr>
2019-05-04 14:31:03 +0900

    circle test

--- app/src/main/java/com/example/pinball/PhysicsEngine.java
+++ app/src/main/java/com/example/pinball/PhysicsEngine.java
@@ -17,8 +17,11 @@ public class PhysicsEngine extends Thread{
 
     // 두 오브젝트가 충돌하면 두 오브젝트의 행동함수를 호출되는 함수
     private void isCollision(PhysicsEngine.PhysicsInterface a, PhysicsEngine.PhysicsInterface b){
-        a.beCollided();
-        b.beCollided();
+        a.beCollided(b);
+    }
+
+    private void collisionCheck(PhysicsInterface a, PhysicsInterface b){
+        a.collisionCheck(b);
     }
 /*
     public PhysicsInterface getView(){
@@ -26,7 +29,9 @@ public class PhysicsEngine extends Thread{
     }
 */
     public interface PhysicsInterface{
-        void beCollided();
+        boolean collisionCheck(PhysicsInterface other);
+        void beCollided(PhysicsInterface other);
+        void act(double px, double py);
     }
 
     public class Grid{
